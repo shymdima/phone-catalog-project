9011ffa0b4e887eb8a68e0846b9c35046dc32812
dima <shymdima74@gmail.com>
2025-04-05 11:11:37 +0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,61 @@
-import React from 'react';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import './App.scss';
+import { Homepage } from './Homepage/Homepage';
+import { Footer } from './Footer/Footer';
+import { TopBar } from './TopBar/TopBar';
+import { ProductDetailsPage } from './ProductDetails/ProductDetails';
+import { Favorites } from './Favorites/Favorites';
+import { Cart } from './Cart/Cart';
+import { Menu } from './Menu/Menu';
+import { ScrollToTop } from './utils/scrollToTop';
+import { NotFound } from './NotFound/NotFound';
+import { ProductPage } from './ProductPage/ProductPage';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+export const App = () => {
+  const location = useLocation();
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <>
+      <div className="app">
+        <ScrollToTop />
+        <TopBar />
+        <main className="app__main">
+          <Routes>
+            <Route path="/home" element={<Navigate to="/" replace />} />
+            <Route path="/" element={<Homepage />} />
+
+            <Route
+              path="/phones"
+              element={<ProductPage pageType="phones" />}
+            ></Route>
+            <Route
+              path="/tablets"
+              element={<ProductPage pageType="tablets" />}
+            ></Route>
+            <Route
+              path="/accessories"
+              element={<ProductPage pageType="accessories" />}
+            ></Route>
+            <Route
+              path="/phones/:productId"
+              element={<ProductDetailsPage productType="phones" />}
+            />
+            <Route
+              path="/tablets/:productId"
+              element={<ProductDetailsPage productType="tablets" />}
+            />
+            <Route
+              path="/accessories/:productId"
+              element={<ProductDetailsPage productType="accessories" />}
+            />
+            <Route path="/favorites" element={<Favorites />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/menu" element={<Menu />}></Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        {location.pathname !== '/menu' && <Footer />}
+      </div>
+    </>
   );
 };
